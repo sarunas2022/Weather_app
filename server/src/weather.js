cba29e91d3b31aaafcbd8f5cc4740884b3898206
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2022-11-10 15:17:08 +0200

    Added 2 routes for current and 5 days weather

---
+++ server/src/weather.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const express = require('express');
+require('dotenv').config();
+
+const router = express.Router();
+
+// route for current weather for chosen location
+router.get('/current', async (req, res) => {
+    try {
+        const { city } = req.body;
+        const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API}`
+        );
+        const currentWeatherData = response.data;
+        res.json(currentWeatherData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+// route for 5 days  weather forecast for chosen location
+router.get('/5_days', async (req, res) => {
+    try {
+        const { city } = req.body;
+        const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.WEATHER_API}`
+        );
+        const forecastWeatherData = response.data;
+        console.log(response);
+        res.json(forecastWeatherData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+module.exports = router;
