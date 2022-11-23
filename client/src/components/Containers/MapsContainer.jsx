16ae241e68d04eb2241ec907e2bb0d43b8866c63
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2022-11-23 21:11:10 +0200

    some changes done to styling

--- client/src/components/Containers/MapsContainer.jsx
+++ client/src/components/Containers/MapsContainer.jsx
@@ -27,9 +27,7 @@ function MapsContainer(props) {
                         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
-                    <Marker position={coordinates}>
-                        {/* <Popup></Popup> */}
-                    </Marker>
+                    <Marker position={coordinates}> </Marker>
                     <RecenterAutomatically coordinates={coordinates} />
                 </MapContainer>
             </div>
