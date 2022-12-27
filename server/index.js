bed5950cc4b660d5b710868263a7d607966e43e4
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2022-12-27 10:49:24 +0200

    change to the server to host in render not  heroku

--- server/index.js
+++ server/index.js
@@ -5,14 +5,14 @@ const weatherRoute = require('./src/weather');
 
 const PORT = process.env.PORT || 3000;
 
-const path = require('path');
+// const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/weather', weatherRoute);
 
 // this is to get all front end data to be taken and used in nodejs
-app.use(express.static(path.join(__dirname + '/public')));
+// app.use(express.static(path.join(__dirname + '/public')));
 
 // second version - not working
 // app.use(express.static('build'));
